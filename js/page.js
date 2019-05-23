d6e50a709df34e4ad9e5c478e642c02979d9fd56
Piotrek Sroka <piotrek@ps-design.pl>
2019-05-23 22:01:01 +0200

    menu-ready

--- js/page.js
+++ js/page.js
@@ -1,6 +1,6 @@
 (function() {
     let toggleMenuBtn = document.querySelector("#toggle-menu");
-    let navigation = document.querySelector(".navigation");
+    let navigation = document.querySelector(".header-nav");
 
     toggleMenuBtn.addEventListener("click", () => {
         navigation.classList.toggle("active");
