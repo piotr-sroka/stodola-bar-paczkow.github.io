3aaa3f4cce2e7d5c4a0664829faa237213ef006c
poncki <piotrek.sroka@zoho.com>
2019-05-24 17:07:30 +0200

    menu and bar

--- js/page.js
+++ js/page.js
@@ -1,8 +1,27 @@
 (function() {
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = function(callback, thisArg) {
+            thisArg = thisArg || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(thisArg, this[i], i, this);
+            }
+        };
+    }
+
     let toggleMenuBtn = document.querySelector("#toggle-menu");
     let navigation = document.querySelector(".header-nav");
+    let navigationItems = document.querySelectorAll(".navigation .navigation-item");
 
-    toggleMenuBtn.addEventListener("click", () => {
+    toggleMenuBtn.addEventListener("click", function() {
         navigation.classList.toggle("active");
+    });
+
+    navigationItems.forEach(function(navItem) {
+        navItem.addEventListener("click", function() {
+            navigationItems.forEach(function(item) {
+                item.classList.remove("active");
+            });
+            this.classList.add("active");
+        });
     });
 })();
